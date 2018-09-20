8af413a42fa53fd26fbac5d4eb17f5287e3caca3
Dallin <wdallin.b271@stu.nebo.edu>
2018-09-20 10:28:18 -0600

    added score and death box

---
+++ 2D_Game/Assets/Scripts/DeathBox.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathBox : MonoBehaviour {
+
+	// Use this for initialization
+    private void OnTriggerEnter2D(Collider2D theObject) {
+        if (theObject.name == "PC") {
+            Debug.Log("Player Enters Death Zone");
+            Destroy(theObject);
+        }
+    }
+	
+}
