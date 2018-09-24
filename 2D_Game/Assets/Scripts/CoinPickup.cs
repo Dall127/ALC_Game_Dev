183f1db55173e95416cae82f54ee8cbe5ef54a34
Dallin <wdallin.b271@stu.nebo.edu>
2018-09-24 10:08:52 -0600

    adding coin pickup

---
+++ 2D_Game/Assets/Scripts/CoinPickup.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class CoinPickup : MonoBehaviour
+{
+
+    public int pointsToAdd;
+
+    void OnTriggerEnter2D(Collider2D theObject)
+    {
+        if (theObject.name == "PC")
+        {
+            Debug.Log("touching");
+
+            ScoreManager.AddPoints(pointsToAdd);
+            Destroy(gameObject);
+        }
+
+    }
+}
+
