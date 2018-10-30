c3925ca5f186f05a86e34a128f7f466310395027
Dallin <wdallin.b271@stu.nebo.edu>
2018-10-30 10:29:20 -0600

    changed camera following

---
+++ 2D_Game/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+    public Player_Control Player;
+    public bool isFollowing;
+
+
+    //camera position offset
+    public float xOffset;
+    public float yOffset;
+
+
+	// Use this for initialization
+	void Start () {
+        Player = FindObjectOfType<Player_Control>();
+        isFollowing = true;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(isFollowing) {
+            transform.position = new Vector3(Player.transform.position.x + xOffset, Player.transform.position.y + yOffset, transform.position.z);
+        }
+	}
+}
